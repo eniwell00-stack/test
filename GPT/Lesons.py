3a31f2c993a8c8cb72375247678b0de1a5ce6a2d
Mathhew <eniwell00@gmail.com>
2026-03-14 20:45:31 +0300

    Калькулятор личных финансов_2

---
+++ GPT/Lesons.py
@@ -0,0 +1,37 @@
+#Task 1
+#name = input("Как вас завут?: ")
+#age = int(input("Сколько вам лет?: "))
+#city = input("Гже вы живете?: ")
+
+#print(f"Привет {name}")
+#print(f"Тебе {age} лет ")
+#print(f"Ты живешь в {city}")
+
+#Task 2
+#width = int(input("Введите ширену прямоугольника: "))
+#height = int(input("Введите высоту прямоугольника: "))
+#print(f'area = {width*height}')
+
+#task 3
+#weight = int(input("Введите свой вес: "))
+#height = float(input("Введите свой рост в см: "))
+#BMI = weight / height ** 2
+#print (f"Ваш ИМТ: {BMI}")
+
+#task 5
+#temp_c = int(input("Введите температуру в градусах C: "))
+#temp_k = 273 + temp_c
+#print(f"Температура в кельвинах: {temp_k}K")
+
+#task 6
+#year = int(input("Введите год своего рождения: "))
+#year_now = int(input("Введите текущий год: "))
+#age = year_now - year
+#print(f'Ваш возраст: {age} года/лет')
+
+#task 7
+#price = int(input("Введите стоимость покупки: "))
+#discount = int(input("Введите скидку в %: "))
+#discounted = (discount/100) * price
+#price_now = price - discounted
+#print(f"Итоговая цена: {price_now}")
---
+++ GPT/Lesos_2.py
@@ -0,0 +1,39 @@
+#Task 1
+#age = int(input("введите свовой возраст: "))
+#if age >= 18:
+#    print("Access allowed")
+#else:
+#    print("Access denied")
+
+#Task 2
+#nums = int(input("Введи число: "))
+#if nums %2 == 0:
+#    print("Even")
+#else:
+#    print("Odd")
+
+#Task 3
+#balls = int(input("Введие свои быллы: "))
+#if balls > 100:
+#    print("Ошибка")
+#elif balls >= 90 :
+#    print ("A")
+#elif balls >= 75:
+#    print ("B")
+#elif balls >= 60:
+#    print ("C")
+#else:
+#    print ("F")
+
+#Task 4
+#nums_one= float(input("Введите первое слогаемое: "))
+#calc = input("Введите операцию (+,-,*,/): ")
+#nums_two = float(input("Введите второе слогаемое: "))
+#if calc == "+":
+#    print (nums_one + nums_two)
+#elif calc == "-":
+#    print(nums_one - nums_two)
+#elif calc == "*":
+#    print(nums_one*nums_two)
+#else:
+#    print(nums_one/nums_two)
